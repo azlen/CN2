757b399c28ebb75222a22288bb0ed422a98e976c
azlen <azlen.elz@gmail.com>
2018-12-15 17:04:03 -0800

    add data

---
+++ www/src/assets/data/sources.md
@@ -0,0 +1,24 @@
+## CC-CEDICT
+[CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0/)
+Chinese -> English dictionary
+
+## Tatoeba
+[CC BY 2.0](https://creativecommons.org/licenses/by/2.0/)
+Community-curated collection of sentences translated between multiple languages
+
+## Make Me a Hanzi
+https://github.com/skishore/makemeahanzi
+Chinese stroke order data and character decompositions
+
+## HSK Vocab
+CC0 1.0 Universal
+
+
+# Future Sources
+
+- HanDeDict (Chinese -> German dictionary)
+- CFDICT (Chinese -> French dictionary)
+- CHDICT (Chinese -> Hungarian dictionary)
+- Open Multilingual Wordnet
+    + Useful for developing categories?
+- zi2zi
--- www/src/components/views/MainPage.vue
+++ www/src/components/views/MainPage.vue
@@ -3,9 +3,9 @@
         <header-component></header-component>
         <div class="filler"></div>
         <div class="paddedContainer">
-            <button-big to="/" title="Recall Mode" description="Test your memory"></button-big>
-            <button-big to="/learn" title="New Vocabulary" description="Start learning 10 new words"></button-big>
-            <button-big to="/" title="Practice" description="141 items to review" class="gradient"></button-big>
+            <button-big to="/" title="学习" description="Review"></button-big>
+            <button-big to="/learn" title="学" description="Learn"></button-big>
+            <!--<button-big to="/" title="Practice" description="141 items to review" class="gradient"></button-big>-->
         </div>
     </div>
 </template>
